d0d84b08578b1f8562e182c11e93155b9b63819c
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-01-24 11:49:10 -0500

    Estructura del proyecto con express

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/paquetes', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/detailView.html'));
+});
+
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en el puerto 3000")
+});
+
+
+
