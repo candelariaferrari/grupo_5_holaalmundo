70505dc0a511463fdbb203dde90a75060c7b1721
Candelaria Ferrari <ferrari.candelaria@gmail.com>
2022-01-26 22:40:31 -0300

    Carrusel - inicio guest

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,31 @@ const navList = document.getElementById("navList");
 toggleButton.addEventListener('click',()=> {
   navList.classList.toggle('active');
 })
+
+/* --------- Carrusel ----------*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) { 
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" activeDot", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activeDot";
+}
