70b09f856542d431ea35a58628b13f1a8985f6d6
Candelaria Ferrari <ferrari.candelaria@gmail.com>
2022-01-27 21:14:55 -0300

    seccion servicios - inicio guest

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,11 @@
 /* boton desplegable responsive */
 const toggleButton = document.getElementById("toggle-button");
 const navList = document.getElementById("navList");
-toggleButton.addEventListener('click',()=> {
+toggleButton.addEventListener('click', () => {
   navList.classList.toggle('active');
 })
 
-/* --------- Carrusel ----------*/
+/* --------- Carrusel inicial ----------*/
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -18,22 +18,60 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) { 
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" activeDot", "");
+    dots[i].className = dots[i].className.replace(" activeDot", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " activeDot";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " activeDot";
 }
 
 /* ---- scroll chat -- */
+const body = document.body;
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+  else if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else {
+    // up
+    body.classList.add(scrollDown);
+    /* body.classList.add(scrollUp); */
+  }
+  lastScroll = currentScroll;
+});
 
+
+/* --- carrusel servicios ----- */
+const serviceContainers = [...document.querySelectorAll('.serviceContainer')];// para que tome todos los servicios dentro de esa etiqueta
+const nxtBtn = document.getElementsByClassName('nxtBtn');
+const preBtn = document.getElementsByClassName('preBtn');
+
+serviceContainers.forEach((item,i)=> {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener('click', ()=>{
+    item.scrollLeft += containerWidth;
+  })
+  preBtn[i].addEventListener('click', ()=>{
+    item.scrollLeft -= containerWidth;
+  })
+})
 
