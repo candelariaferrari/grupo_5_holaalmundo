9aaaf0fda5601786ecbc2dff415ccec5d6e2d63e
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-01-31 19:01:28 -0500

    Se ajusta perfil profesores

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.static('public'));
 
+/** VISTA USUARIO */
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/inicioGuests.html'));
 });
@@ -23,6 +25,18 @@ app.get('/reserva', (req, res) => {
 app.get('/carritoCompras', (req, res) => {
     res.sendFile(path.join(__dirname, './views/carritoCompras.html'));
 });
+
+/** VISTA PROFESOR */
+
+app.get('/perfilProfesores1', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/seteoPerfilProfes-1.html'));
+});
+
+app.get('/perfilProfesores2', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/seteoPerfilProfes-2.html'));
+});
+
+/** SERVIDOR */
 
 app.listen(3000, () => { 
     console.log("Servidor corriendo en puerto 3000");
