7843a9e27c903c5f34d2fc82633603203eb7a4f5
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-04 03:04:23 -0300

    aclaraciones sales.js

--- src/database/models/sales.js
+++ src/database/models/sales.js
@@ -1,36 +1,42 @@
-module.exports = (sequelize, dataTypes) => {
-    let alias = 'Sale'; // esto debería estar en singular
-    let cols = {
-        idSale: {
-            type: dataTypes.BIGINT(10).UNSIGNED,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
+// CREO Y EXPORTO EL MODELO 
+
+module.exports = function(sequelize, dataTypes) 
+{
+let alias = 'Sale'
+
+let cols = 
+{
+idSale: {
+         type: dataTypes.BIGINT(10).UNSIGNED,
+         primaryKey: true,
+         allowNull: false,
+         autoIncrement: true
         },
-        description: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        total: {
-            type: dataTypes.BIGINT,
-            allowNull: false
-        }
-    };
-    let config = {
-        tableName: 'Sale',
-        timestamps: false,
-    }
+description: {
+              type: dataTypes.STRING,
+              allowNull: false
+             },
+total: {
+        type: dataTypes.BIGINT,
+        allowNull: false
+       }
+}
 
-    const Sale = sequelize.define(alias,cols,config);
+let config = {
+              tableName: "Sale",
+              timestamps: false,
+             }
 
-    //Aquí debes realizar lo necesario para crear las relaciones con los otros modelos
-    Sale.associate = function(models){
-        Sale.hasMany(models.Class, {
-            as: "classCost",
-            foreignKey: "sale_id"
-        });
-    }
+const Sale = sequelize.define(alias,cols,config)
 
-    return Sale
+// RELACIONES ENTRE MODELOS 
+    
+Sale.associate = function(models)
+{
+Sale.hasMany(models.Class, {as: "classCost",
+                            foreignKey: "sale_id"})
+}
+
+return Sale
 
 };
