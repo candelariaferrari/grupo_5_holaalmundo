74751785e4c44f453d8945d1a238c1f275af30b1
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-04 03:21:38 -0300

    aclaraciones user_class.js

--- src/database/models/user_class.js
+++ src/database/models/user_class.js
@@ -1,39 +1,49 @@
-module.exports = (sequelize, dataTypes) => {
-    let alias = 'User_Class'; // esto debería estar en singular
-    let cols = {
-        idUsuarios_Clases: {
-            type: dataTypes.BIGINT(10).UNSIGNED,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
-        id_user: {
-            type: dataTypes.INTEGER,
-            allowNull: false
-        },
-        id_class: {
-            type: dataTypes.INTEGER,
-            allowNull: false
-        }
-    };
-    let config = {
-        timestamps: false,
-    }
+// CREO Y EXPORTO EL MODELO 
 
-    const User_Class = sequelize.define(alias,cols,config);
+module.exports = function(sequelize, dataTypes) 
+{
+let alias = 'User_Class'
 
-    //Aquí debes realizar lo necesario para crear las relaciones con los otros modelos
-    /*User_Class.associate = function(models){
-        User_Class.hasMany(models.User, {
-            as: "users",
-            foreignKey: "id_user"
-        });
-        User_Class.hasMany(models.Class, {
-            as: "classes",
-            foreignKey: "id_class"
-        });
-    }*/
+let cols = 
+{
+idUsuarios_Clases: {
+                    type: dataTypes.BIGINT(10).UNSIGNED,
+                    primaryKey: true,
+                    allowNull: false,
+                    autoIncrement: true
+                   },
+id_user: {
+          type: dataTypes.INTEGER,
+          allowNull: false
+         },
+id_class: {
+           type: dataTypes.INTEGER,
+           allowNull: false
+          }
+}
 
-    return User_Class;
+let config = {
+              timestamps: false,
+             }
+
+const User_Class = sequelize.define(alias,cols,config)
+
+// RELACIONES ENTRE MODELOS 
+
+/*
+User_Class.associate = function(models)
+{
+User_Class.hasMany(models.User, {as: "users",
+                                 foreignKey: "id_user"})
+
+User_Class.hasMany(models.Class, {as: "classes",
+                                  foreignKey: "id_class"})
+}
+*/
+
+return User_Class
+
+};
+
 
 };
