cd8cbe1b4a28afb355637c0ace59c1efa57d44d0
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-04 01:59:27 -0300

    aclaraciones studentsController

--- src/controllers/studentsController.js
+++ src/controllers/studentsController.js
@@ -1,10 +1,14 @@
+// CONSTANTES
+
 const path = require('path');
-//const teacherServices = require("../services/teachersService");
-//const packageService = require("../services/packagesService");
-//const commentService = require("../services/commentService");
+const teacherServices = require("../services/teachersService");
+const packageService = require("../services/packagesService");
+const commentService = require("../services/commentService");
 const db = require('../database/models');
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
+
+// BASES DE DATOS
 
 const Class = db.Class;
 const users = db.User;
@@ -12,150 +16,109 @@ const comment = db.Comment;
 const sex = db.Sex;
 const rol = db.Rol;
 
+// CONTROLADOR
+
 let studentsController = {
 
-/**  Aqui van los metodos que se encargan de manejar a los estudiantes*/
-
     home: function(req, res) {
+          //const servicios = packageService.findAllServices();
+          //const profesores = teacherServices.findAllTeachers();
+          //const serviciosRecomendados = packageService.findAllSuggest();
+          //const serviciosMasVendidos = packageService.findAllSold();
         
-        //const servicios = packageService.findAllServices();
-        //const profesores = teacherServices.findAllTeachers();
-        //const serviciosRecomendados = packageService.findAllSuggest();
-        //const serviciosMasVendidos = packageService.findAllSold();
-        
-        /*res.render('students/homeStudents', {servicios: servicios, 
-                                        serviciosRecomendados: serviciosRecomendados,
-                                        serviciosMasVendidos: serviciosMasVendidos,
-                                        profesores: profesores, 
-                                        user: res.locals.userLogged});*/
+          /*res.render('students/homeStudents', {servicios: servicios, 
+                                                 serviciosRecomendados: serviciosRecomendados,
+                                                 serviciosMasVendidos: serviciosMasVendidos,
+                                                 profesores: profesores, 
+                                                 user: res.locals.userLogged});*/
 
-        const servicios = Class.findAll(
-            {
-                // Como se haria el include en la vista de home para imprimer el servicio.sale_class.total
-                include: [{association: "sale_class"}]
-            }
-        )
-        const cantidadTemas = Class.findAll(
-            {
-                include: [
-                    {
-                        // Como opera en un cruce de tablas el where (deberia aparecer uno)
-                        association: "tematica"
-                    }
-                ]
-            }, 
-            {
-                attributes: [
-                    [sequelize.fn('DISTINCT', sequelize.col(`tematica.idTopic`)), `countTopics`]
-                ]
-            }
-        )
-        const cantidadIdiomas = Class.findAll(
-            {
-                attributes: [
-                    [sequelize.fn('DISTINCT', sequelize.col(`language`)), `countLanguage`]
-                ]
-            }
-        )
-        const profesores = users.findAll(
-            {
-                include: [
-                        {
-                            // Como opera en un cruce de tablas el where (deberia aparecer uno)
-                            association: "rol"
-                        }
-                ],
-                where: {
-                    rol_id: 2
-                }
-            }
-        )
-        const serviciosRecomendados = Class.findAll(
-            {
-                where: {
-                    visited: 1
-                }
-            }
-        )
-        const serviciosMasVendidos = Class.findAll(
-            {
-                include: [
-                        {
-                            association: "sale_class"
-                        }
-                ],
-                // Where sale_class.total > 100
-                where: {
-                    price: {
-                        [Op.gt]: 300 
-                    }
-                }
-            }
-        )
-        Promise.all([servicios, profesores, cantidadIdiomas, cantidadTemas, serviciosRecomendados, serviciosMasVendidos])
-            .then(function([servicios, profesores, cantidadIdiomas, cantidadTemas, serviciosRecomendados, serviciosMasVendidos]){
+          const servicios = Class.findAll({include: [{association: "sale_class"}]})
+          
+          const cantidadTemas = Class.findAll({include: [{association: "tematica"}]}, 
+                                              {attributes: [[sequelize.fn('DISTINCT', sequelize.col(`tematica.idTopic`)), `countTopics`]]})
+          
+          const cantidadIdiomas = Class.findAll({attributes: [[sequelize.fn('DISTINCT', sequelize.col(`language`)), `countLanguage`]]})
+          
+          const estudiantes = users.findAll({include: [{association: "rol"}],
+                                             where: {rol_id: 1}})
+          
+          const profesores = users.findAll({include: [{association: "rol"}],
+                                            where: {rol_id: 2}})
+          
+          const serviciosRecomendados = Class.findAll({where: {visited: 4}})
+          
+          const serviciosMasVendidos = Class.findAll({include: [{association: "sale_class"}],
+                                                      where: {price: {[Op.gt]: 1000}}})
+          
+          Promise.all([servicios, profesores, estudiantes, cantidadIdiomas, cantidadTemas, serviciosRecomendados, serviciosMasVendidos])
+                  
+                 .then(function([servicios, profesores, estudiantes, cantidadIdiomas, cantidadTemas, serviciosRecomendados, serviciosMasVendidos]){
+                       /*serviciosRecomendados: serviciosRecomendados,
+                         serviciosMasVendidos: serviciosMasVendidos,
+                         user: res.locals.userLogged*/
                 
-                /*serviciosRecomendados: serviciosRecomendados,
-                serviciosMasVendidos: serviciosMasVendidos,
-                user: res.locals.userLogged*/
-                
-                res.render('students/homeStudents', {servicios: servicios, 
-                                                    profesores: profesores, 
-                                                    cantidadIdiomas:cantidadIdiomas, 
-                                                    cantidadTemas:cantidadTemas,
-                                                    serviciosRecomendados,
-                                                    serviciosMasVendidos: serviciosMasVendidos})
-            })
-            .catch(error => {
-                console.log(error);
-        });
+                 res.render('students/homeStudents', {servicios: servicios, 
+                                                      profesores: profesores, 
+                                                      estudiantes: estudiantes,
+                                                      cantidadIdiomas:cantidadIdiomas, 
+                                                      cantidadTemas:cantidadTemas,
+                                                      serviciosRecomendados,
+                                                      serviciosMasVendidos: serviciosMasVendidos})})
+        
+                 .catch(error => {console.log(error)});
     }, 
-    createComment: function(req, res){
-        //commentService.createComment(req.body);
-        //res.redirect('/home');
-        comment.create({
-            nombres: req.body.nameContact,
-            email: req.body.emailContact,
-            description: req.body.consultaContact
-        })
-        .then(() =>{
-            res.redirect('/');
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    createComment: function(req, res) {
+                   //commentService.createComment(req.body);
+                   //res.redirect('/home');
+        
+                   comment.create({nombres: req.body.nameContact,
+                                   email: req.body.emailContact,
+                                   description: req.body.consultaContact})
+        
+                          .then(() => {res.redirect('/')})
+        
+                          .catch(error => {console.log(error)});
     },
     services: function(req, res) {
-        const profesores = teacherServices.findAllTeachers();
-        const serviciosRecomendados = packageService.findAllSuggest();
-        const serviciosMasVendidos = packageService.findAllSold();
-        const servicios = packageService.findAllServices();
+              
+              const profesores = teacherServices.findAllTeachers();
         
-        res.render('students/packageStudents', {profesores: profesores, 
-                                                servicios: servicios,
-                                                serviciosRecomendados: serviciosRecomendados,
-                                                serviciosMasVendidos:serviciosMasVendidos});
+              const serviciosRecomendados = packageService.findAllSuggest();
+              
+              const serviciosMasVendidos = packageService.findAllSold();
+        
+              const servicios = packageService.findAllServices();
+        
+              res.render('students/packageStudents', {profesores: profesores, 
+                                                      servicios: servicios,
+                                                      serviciosRecomendados: serviciosRecomendados,
+                                                      serviciosMasVendidos:serviciosMasVendidos});
     },
     filterTeachers: function(req, res) {
-        res.render('students/viewTeachers');
-    },
-    configuracion: function(req, res) {       
+                    
+                    res.render('students/viewTeachers');
     },
     reserva: function(req, res) {
-        res.render('partials/popUpReserve');
+        
+             res.render('partials/popUpReserve');
     }, 
     configuration: function(req, res) {
 
-        res.render('students/configurationStudents');
+                   res.render('students/configurationStudents');
     }, 
     shoppingCart: function(req, res) {
-        res.render('shoppingCart/shoppingCart');
+        
+                  res.render('shoppingCart/shoppingCart');
     }, 
     logout: function(req, res) {
-        res.clearCookie('userEmail');
-        req.session.destroy();
-        console.log(req.session);
-        return res.redirect('/');
+            
+            res.clearCookie('userEmail');
+            req.session.destroy();
+            console.log(req.session);
+            return res.redirect('/');
     }
 };
+
+// EXPORTO EL CONTROLADOR
+
 module.exports = studentsController;
