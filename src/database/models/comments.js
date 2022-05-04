a629e7c4ffa6cb4ed345a72d083342bb7f6c8951
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-04 02:47:02 -0300

    aclaraciones comments.js

--- src/database/models/comments.js
+++ src/database/models/comments.js
@@ -1,40 +1,48 @@
-module.exports = (sequelize, dataTypes) => {
-    let alias = 'Comment'; // esto debería estar en singular
-    let cols = {
-        idComment: {
+// CREO Y EXPORTO EL MODELO 
+
+module.exports = function(sequelize, dataTypes)  
+{
+let alias = 'Comment'
+
+let cols = 
+{
+idComment: {
             type: dataTypes.BIGINT(10).UNSIGNED,
             primaryKey: true,
             allowNull: false,
             autoIncrement: true
-        },
-        nombres: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: dataTypes.STRING,
-            allowNull: false
-        }
-    };
-    let config = {
-        tableName: "Comment",
-        timestamps: false,
-    }
+           },
+nombres: {
+          type: dataTypes.STRING,
+          allowNull: false
+         },
+email: {
+        type: dataTypes.STRING,
+        allowNull: false
+       },
+description: {
+              type: dataTypes.STRING,
+              allowNull: false
+             }
+}
 
-    const Comment = sequelize.define(alias,cols,config);
+let config = {
+              tableName: "Comment",
+              timestamps: false,
+             }
 
-    //Aquí debes realizar lo necesario para crear las relaciones con los otros modelos
-    /*Comment.associate = function(models){
-        Comment.belongsTo(models.User, {
-            as: "user",
-            foreignKey: "idUsuarioFK"
-        });
-    }*/
+const Comment = sequelize.define(alias,cols,config)
 
-    return Comment;
+// RELACIONES ENTRE MODELOS 
+
+/*
+Comment.associate = function(models)
+{
+Comment.belongsTo(models.User, {as: "user",
+                                foreignKey: "idUsuarioFK"})
+}
+*/
+
+return Comment
 
 };
