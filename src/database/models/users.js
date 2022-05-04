1ee4c631861692e262da5331796cd32b2c95157e
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-04 03:31:47 -0300

    aclaraciones users.js

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -1,79 +1,83 @@
-module.exports = (sequelize, dataTypes) => {
-    let alias = 'User'; // esto debería estar en singular
-    let cols = {
-        idUsuario: {
+// CREO Y EXPORTO EL MODELO 
+
+module.exports = function(sequelize, dataTypes) 
+{
+let alias = 'User'
+    
+let cols = 
+{
+idUsuario: {
             type: dataTypes.BIGINT(10).UNSIGNED,
             primaryKey: true,
             allowNull: false,
             autoIncrement: true
+           },
+name: {
+       type: dataTypes.STRING,
+       allowNull: false
+      },
+lastName: {
+           type: dataTypes.STRING,
+           allowNull: false
+          },
+email: {
+        type: dataTypes.STRING,
+        allowNull: false
+       },
+phone: {
+        type: dataTypes.INTEGER,
+        allowNull: false
+       },
+password: {
+           type: dataTypes.STRING,
+           allowNull: false
+          },
+validationPassword: {
+                     type: dataTypes.STRING,
+                     allowNull: false
+                    },
+sex_id: {
+         type: dataTypes.INTEGER,
+         allowNull: false
         },
-        name: {
-            type: dataTypes.STRING,
-            allowNull: false
+rol_id: {
+         type: dataTypes.INTEGER,
+         allowNull: false
         },
-        lastName: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        phone: {
-            type: dataTypes.INTEGER,
-            allowNull: false
-        },
-        password: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        validationPassword: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        sex_id: {
-            type: dataTypes.INTEGER,
-            allowNull: false
-        },
-        rol_id: {
-            type: dataTypes.INTEGER,
-            allowNull: false
-        },
-        image: {
-            type: dataTypes.STRING,
-            allowNull: true
-        },
-        terms_condition: {
-            type: dataTypes.STRING,
-            allowNull: false
-        }
+image: {
+        type: dataTypes.STRING,
+        allowNull: true
+       },
+terms_condition: {
+                  type: dataTypes.STRING,
+                  allowNull: false
+                 }
+}
 
-    };
-    let config = {
-        tableName: "User",
-        timestamps: false,
-    }
+let config = {
+              tableName: "User",
+              timestamps: false,
+             }
 
-    const User = sequelize.define(alias,cols,config);
+const User = sequelize.define(alias,cols,config)
 
-    //Aquí debes realizar lo necesario para crear las relaciones con los otros modelos
-    User.associate = function(models){
-        User.belongsTo(models.Sex, {
-            as: "sexo",
-            foreignKey: "sex_id"
-        });
-        User.belongsTo(models.Rol, {
-            as: "rol",
-            foreignKey: "rol_id"
-        });
-        /** Como seria la relacion con la tabla intermedia */
-        User.belongsToMany(models.Class, {
-            as: "classes",
-            through: "Usuarios_Clases",
-            foreignKey: "id_user",
-            otherKey: "id_class",
-            timestamps: false,
-        });
-    }
-    return User;
+// RELACIONES ENTRE MODELOS 
+
+User.associate = function(models)
+{
+User.belongsTo(models.Sex, {as: "sexo",
+                            foreignKey: "sex_id"})
+
+User.belongsTo(models.Rol, {as: "rol",
+                            foreignKey: "rol_id"})
+
+User.belongsToMany(models.Class, {as: "classes",
+                                  through: "Usuarios_Clases",
+                                  foreignKey: "id_user",
+                                  otherKey: "id_class",
+                                  timestamps: false})
+}
+
+return User
+    
 };
