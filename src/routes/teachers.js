3be68a393628f64033ccab6526dbefb51f28fe1b
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-05 03:39:17 -0300

    aclaraciones teachers.js

--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -1,20 +1,32 @@
-// ************ Require's ************
+// CONSTANTES
+
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-
-// ************ Controller Require ************
 const teacherController = require('../controllers/teachersController');
-const authMiddlewars = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-/* Profes */
-router.get('/home', authMiddlewars, teacherController.home); //funciona
-router.get('/students', teacherController.students); //funciona
-router.get('/packages', teacherController.packages); //funcona
-router.get('/configuration', teacherController.configuration); //funciona
-router.get('/dashboardLessons', teacherController.dashboardLessons);//funciona
-/*** LOGOUT */
+// RUTAS 
+
+// INICIO DE PROFESORES
+router.get('/home', authMiddleware, teacherController.home); 
+
+// OBTENER TODOS LOS ESTUDIANTES PARA LOS PROFESORES
+router.get('/students', teacherController.students); 
+
+// OBTENER TODOS LOS PAQUETES DE LOS PROFESORES
+router.get('/packages', teacherController.packages); 
+
+// CONFIGURACIONES DEL PERFIL DE LOS PROFESORES
+router.get('/configuration', teacherController.configuration); 
+
+// CLASES DE LOS PROFESORES
+router.get('/dashboardLessons', teacherController.dashboardLessons);
+
+// CERRAR SESIÓN DEL PROFESOR 
 router.get('/logout', teacherController.logout);
+
+// EXPORTAR EL MODULO 
 
 module.exports = router;
