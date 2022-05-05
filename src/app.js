848863ca9c252ece5b8b506355011b7872c75c52
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-05 03:57:15 -0300

    aclaraciones app.js

--- src/app.js
+++ src/app.js
@@ -1,68 +1,59 @@
-// ************ Require's ************
+// CONSTANTES
+
 const createError = require('http-errors');
 const cookies = require('cookie-parser');
 const express = require('express');
+const app = express();
 const logger = require('morgan');
 const path = require('path');
-const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
-const session = require("express-session");
-const userLoggedMiddlware = require('./middlewares/userLoggedMiddlware');
-const rutasMain = require('./routes/main');
-const routsStudents = require('./routes/students');
-const routTeachers = require('./routes/teachers');
+const methodOverride =  require('method-override');                                                  // Para poder usar los métodos PUT y DELETE
+const session = require('express-session');
+const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
+const routesMain = require('./routes/main');
+const routesStudents = require('./routes/students');
+const routesTeachers = require('./routes/teachers');
 
-// ************ express() - (don't touch) ************
-const app = express();
+// MÉTODOS 
 
-// ************ Middlewares - (don't touch) ************
-app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
-app.use(express.urlencoded({ extended: false })); //Necesario para tener el req.body
+app.use(express.static(path.join(__dirname, '../public')));                                          // Necesario para los archivos estáticos en el folder /public
+app.use(express.urlencoded({ extended: false }));                                                    // Necesario para obtener el req.body
 app.use(logger('dev'));
 app.use(express.json());
-app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
-
-app.use(session({
-  secret:"Secretoo breoo!!",
-  resave: true ,
-  saveUninitialized: true 
-}));
-
+app.use(methodOverride('_method'));                        
+app.use(session({secret: 'Secreto',
+                 resave: true ,
+                 saveUninitialized: true}));
 app.use(cookies());
+app.use(userLoggedMiddleware);
 
-app.use(userLoggedMiddlware);
+// TEMPLATE ENGINE 
 
-// ************ Template Engine - (don't touch) ************
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// ************ Route System require and use() ************
-app.use('/', rutasMain);
-app.use('/students', routsStudents);
-app.use('/teachers', routTeachers);
+// RUTAS
 
-// ************ Template Engine - (don't touch) ************
+app.use('/', routesMain);
+app.use('/students', routesStudents);
+app.use('/teachers', routesTeachers);
+
+// TEMPLATE ENGINE 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-// ************ DON'T TOUCH FROM HERE ************
-// ************ catch 404 and forward to error handler ************
+// ERROR 404
+
 app.use((req, res, next) => next(createError(404)));
 
-// ************ error handler ************
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.path = req.path;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// MANEJADOR DE ERRORES 
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use((err, req, res, next) => {res.locals.message = err.message;
+                                  res.locals.path = req.path;
+                                  res.locals.error = req.app.get('env') === 'development' ? err : {};
+                                  res.status(err.status || 500);
+                                  res.render('error');});
 
-// ************ Servidor ************
+// SERVIDOR
 
-app.listen(3000, () => { 
-    console.log("Servidor corriendo en puerto 3000");
-});
-
+app.listen(3000, () => {console.log("Servidor corriendo en el puerto 3000!");});
