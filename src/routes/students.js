571b5cce3e7b4c02c847476d9f6ee3650f45e67f
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-31 01:57:05 -0300

    actualizacion rutas

--- src/routes/students.js
+++ src/routes/students.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const studentsController = require('../controllers/studentsController');
 const authMiddlewars = require('../middlewares/authMiddleware');
+const validator = require('../middlewares/express-validator');
 //const adminMiddlware = require('../middlewares/adminMiddleware');
 
 /*** GET all info in Home Students ***/ 
@@ -19,6 +20,7 @@ router.get('/packages', authMiddlewars, studentsController.services); // authMid
 
 /*** PROFIL PROFESORES */
 router.get('/configuration', authMiddlewars, studentsController.configuration); 
+router.post('/configuration', validator.configurationStudents, studentsController.configurationProcess)
 
 /*** SHOPPING CART  */
 router.get('/shoppingCart', authMiddlewars, studentsController.shoppingCart); 
@@ -30,3 +32,4 @@ router.get('/detailsTeacher', authMiddlewars, studentsController.detailsTeacher)
 router.get('/logout', studentsController.logout);
 
 module.exports = router;
+
