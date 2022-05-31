cada64898ad746f3fa4e2fa93e5a4fd63ba69587
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-05-31 01:55:25 -0300

    actualizacion express-validator

--- src/middlewares/express-validator.js
+++ src/middlewares/express-validator.js
@@ -1,4 +1,4 @@
-const { check, body } = require("express-validator");
+const { check } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const db = require('../database/models');
 const userServices = require("../services/usersService");
@@ -6,28 +6,24 @@ const userServices = require("../services/usersService");
 const validar = {
 
   register: [
-    body('name')
+    check("name")
       .notEmpty()
-      .withMessage('Tines que ingresar el nombre')
-      .bail()
+      .withMessage("Ingresa tu nombre")
       .isLength({ min: 3 })
-      .withMessage("Campo de nombre debe tener un minimo de 3 caracteres"),
-    body('last_name')
+      .withMessage("El campo nombre debe tener un mínimo de 3 caracteres"),
+    check("last_name")
       .notEmpty()
-      .withMessage('Tines que ingresar el apellido')
-      .bail()
+      .withMessage("Ingresa tu apellido")
       .isLength({ min: 3 })
-      .withMessage("Campo de apellido debe tener un minimo de 3 caracteres"),
-    body('phone')
+      .withMessage("El campo apellido debe tener un mínimo de 3 caracteres"),
+    check("phone")
       .notEmpty()
-      .withMessage('Tines que ingresar el celular')
-      .bail()
+      .withMessage("Ingresa tu número de celular")
       .isNumeric({ no_symbols: true })
-      .withMessage("Debes incluir exclusivamente números"),
-    body('email')
+      .withMessage("El campo celular debe tener únicamente números"),
+    check("email")
       .notEmpty()
-      .withMessage('Tines que ingresar un email')
-      .bail()
+      .withMessage("Ingresa tu email")
       .custom((value, { req }) => {
 
         return db.User.findOne({
@@ -36,27 +32,25 @@ const validar = {
           }
         }).then(user => {
           if (user) {
-            return Promise.reject("Email ya registrado!")
+            return Promise.reject("El email ya fue registrado")
           }
         })
       }),
-    body('password')
+    check("password")
       .notEmpty()
-      .withMessage('Tines que ingresar la contraseña')
-      .bail()
+      .withMessage("Ingresa tu contraseña")
       .isLength({ min: 4 })
-      .withMessage("La contraseña debe tener un minimo de 4 caracteres")
+      .withMessage("La contraseña debe tener un mínimo de 8 caracteres")
       .custom((value, { req }) => {
         let password = req.body.password;
 
-        // let regex = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/
         mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
         numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         caracteresEspeciales = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])/
 
         if (!password) {
-          throw new Error('Tienes que digital tu contraseña');
+          throw new Error("Complete el campo de la contraseña")
         } else {
           let contadorMayuscula = 0;
           let contadorMinusculas = 0;
@@ -82,56 +76,51 @@ const validar = {
             return true;
           } else {
             if (contadorMayuscula == 0)
-              throw new Error('Debes ingresar minimo una letra Mayuscula');
+              throw new Error("Tu contraseña debe contener al menos una letra mayúscula")
             if (contadorMinusculas == 0)
-              throw new Error('Debes ingresar minimo una letra Minuscula');
+              throw new Error("Tu contraseña debe contener al menos una letra minúscula")
             if (contadorNumeros == 0)
-              throw new Error('Debes ingresar minimo un numero');
+              throw new Error("Tu contraseña debe contener al menos un número")
             if (caracteresEspeciales == 0)
-              throw new Error('Debes ingresar minimo un caracter especial');
+              throw new Error("Tu contraseña debe contener al menos un carácter especial")
           }
         }
       }),
-    body('validationPassword')
+    check("validationPassword")
       .notEmpty()
-      .withMessage('Tines que ingresar la misma contraseña')
+      .withMessage("Vuelve a ingresar tu contraseña")
       .custom((value, { req }) => {
         let password = req.body.password;
 
         let validationPassword = req.body.validationPassword;
 
         if (!password) {
-          throw new Error('Tienes que confirmar tu contraseña');
+          throw new Error("Confirma tu contraseña")
         } else {
           for (let i = 0; i < password.length; i++) {
             if (password[i] == validationPassword[i]) {
               continue;
             } else {
-              throw new Error('Las contraseñas no son las mismas');
+              throw new Error("Las contraseñas ingresadas no coinciden")
             }
           }
           return true;
         }
       }),
-    body('gender')
+    check("gender")
       .notEmpty()
-      .withMessage('Tines que ingresar el genero')
-      .bail(),
-    body('userRol')
+      .withMessage("Selecciona tu género"),
+    check("userRol")
       .notEmpty()
-      .withMessage('Tines que ingresar un usuario')
-      .bail(),
+      .withMessage("Selecciona tu rol"),
   ],
 
   login: [
-    /** Se toma el campo de name del formulario */
-    body('email')
-      .notEmpty()
-      .withMessage('Tines que ingresar el email')
-      .bail()
+    check("email")
+      .notEmpty() 
+      .withMessage("Ingresa tu email")
       .isEmail()
-      .withMessage("Email con formato incorrecto")
-      .bail()
+      .withMessage("El campo email debe tener una dirección de correo válida")
       .custom((value, { req }) => {
 
         return db.User.findOne({
@@ -140,13 +129,13 @@ const validar = {
           }
         }).then(user => {
           if (!user) {
-            return Promise.reject("Usuario invalido")
+            return Promise.reject("El usuario no es válido")
           }
         })
       }),
-    body('password')
+    check("password")
       .notEmpty()
-      .withMessage('Tines que ingresar la contraseña')
+      .withMessage("Ingresa tu contraseña")
       .custom((value, { req }) => {
 
         return db.User.findOne({
@@ -156,7 +145,7 @@ const validar = {
         }).then(user => {
 
           if (!user) {
-            return Promise.reject("Usuario invalido")
+            return Promise.reject("El usuario no es válido")
           } else if (!bcrypt.compareSync(req.body.password, user.password)) {
             return Promise.reject("La contraseña no corresponde al email ingresado")
           }
@@ -166,43 +155,80 @@ const validar = {
  createPackages: [
     check("description")
       .notEmpty()
-      .withMessage("Porfavor escriba el nombre de su paquete")
-      .bail()
+      .withMessage("Ingresa el nombre de tu paquete")
       .isLength({ max: 20 })
-      .withMessage("campo nombre debe superar los 20 caracteres"),
-    check('language')
+      .withMessage("El nombre no debe superar los 20 caracteres"),
+    check("language")
       .notEmpty()
-      .withMessage('Tines que seleccionar un idioma')
-      .bail(),
-    check('week_days')
+      .withMessage("Selecciona un idioma"),
+    check("week_days")
       .notEmpty()
-      .withMessage('Tines que seleccionar un grupo de dias')
-      .bail(),
-    check('week_times')
+      .withMessage("Selecciona una combinación de días"),
+    check("week_times")
       .notEmpty()
-      .withMessage('Tines que seleccionar un horario'),
-    check('level')
+      .withMessage("Selecciona un horario"),
+    check("level")
       .notEmpty()
-      .withMessage('Tines que seleccionar un nivel'),
-    check('topics')
+      .withMessage("Selecciona un nivel"),
+    check("topics")
       .notEmpty()
-      .withMessage('Tines que seleccionar una tematica'),
-    check('types')
+      .withMessage("Selecciona una temática"),
+    check("types")
       .notEmpty()
-      .withMessage('Tines que seleccionar el tipo de modalidad'),
+      .withMessage("Selecciona una modalidad"),
     check("price")
       .notEmpty()
-      .withMessage("Debe colocar un precio de paquete")
+      .withMessage("Ingresa el precio de tu paquete")
       .isNumeric({ no_symbols: true })
-      .withMessage("campo nombre debe contener simbolos"),
+      .withMessage("El campo precio no debe contener símbolos"),
     check("cap_max")
       .notEmpty()
-      .withMessage("Debe colocar un precio de paquete")
+      .withMessage("Ingresa una cantidad máxima de alumnos")
       .isNumeric({ no_symbols: true })
-      .withMessage("campo nombre debe contener simbolos"),
+      .withMessage("El campo capacidad máxima no debe contener símbolos"),
     check("link_class")
       .notEmpty()
-      .withMessage("Debe colocar un enlace de plataforma")
+      .withMessage("Ingresa el link hacia una plataforma")
+  ],
+  configurationStudents: [
+    check("name")
+      .notEmpty()
+      .withMessage("Ingresa tu nombre")
+      .isLength({ min: 3 })
+      .withMessage("El campo nombre debe tener un mínimo de 3 caracteres"),
+    check("last_name")
+      .notEmpty()
+      .withMessage("Ingresa tu apellido")
+      .isLength({ min: 3 })
+      .withMessage("El campo apellido debe tener un mínimo de 3 caracteres"),
+    check("phone")
+      .notEmpty()
+      .withMessage("Ingresa tu número de celular")
+      .isNumeric({ no_symbols: true })
+      .withMessage("El campo celular debe tener únicamente números"),
+    check("avatar")
+      .notEmpty()
+      .withMessage("Carga una foto para tu perfil")
+  ],
+  configurationTeachers: [
+    check("name")
+      .notEmpty()
+      .withMessage("Ingresa tu nombre")
+      .isLength({ min: 3 })
+      .withMessage("El campo nombre debe tener un mínimo de 3 caracteres"),
+    check("last_name")
+      .notEmpty()
+      .withMessage("Ingresa tu apellido")
+      .isLength({ min: 3 })
+      .withMessage("El campo apellido debe tener un mínimo de 3 caracteres"),
+    check("phone")
+      .notEmpty()
+      .withMessage("Ingresa tu número de celular")
+      .isNumeric({ no_symbols: true })
+      .withMessage("El campo celular debe tener únicamente números"),
+    check("avatar")
+      .notEmpty()
+      .withMessage("Carga una foto para tu perfil")
   ]
 }
 
