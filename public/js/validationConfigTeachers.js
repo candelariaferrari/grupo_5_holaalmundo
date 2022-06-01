e54dc873d34e6b7d27b280db14d74aca51304522
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-06-01 19:41:33 -0300

    actualizo validationConfigTeachers

--- public/js/validationConfigTeachers.js
+++ public/js/validationConfigTeachers.js
@@ -1,7 +1,6 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function () 
+{
   let form = document.getElementById("config-teachers")
-  let name = document.getElementById("nameTeacher")
-  let last_name = document.getElementById("last_nameTeacher")
   let phone = document.getElementById("phoneTeacher")
 
   let errors = []
@@ -9,63 +8,63 @@ window.addEventListener("load", function () {
 
   console.log(ulErrors + "llegue?")
 
-  form.addEventListener('submit', (e) => {
+  form.addEventListener('submit', (e) => 
+  {
     e.preventDefault()
 
-    if (name.value == '') {
-      console.log(name.value)
-      name.classList.toggle("backgroundFocus")
-      errors.push("El campo Nombre no puede estar vacío")
-    }
-    else { name.classList.remove("backgroundFocus") }
-
-    if (last_name.value == '') {
-      errors.push("El campo Apellido no puede estar vacío")
-      last_name.classList.toggle("backgroundFocus")
-    }
-    else { last_name.classList.remove("backgroundFocus") }
-
-    if (phone.value == '') {
+    if (phone.value == '') 
+    {
       errors.push("El campo Celular no puede estar vacío")
       phone.classList.toggle("backgroundFocus")
     }
     else { phone.classList.remove("backgroundFocus") }
 
     /* --- errores --- */
-    if (errors.length > 0) {
+
+    if (errors.length > 0) 
+    {
       e.preventDefault()
+
       ulErrors.innerHTML = ""
-      for (let i = 0; i < errors.length; i++) {
+
+      for (let i = 0; i < errors.length; i++) 
+      {
         ulErrors.innerHTML += `<div>* <li> ${errors[i]} </li> </div>`
       }
       errors = ""
     }
-    else {
+    else 
+    {
       ulErrors.innerHTML = ""
       form.submit()
     }
     console.log(errors)
   })
-
 })
 
-function imageValidation() {
+function imageValidation() 
+{
   let image = document.getElementById("nuestroInputTeacher")
   let imagePreview = document.getElementById("nuestroInputPreview")
   var filePath = image.value
   var allowedExtensions = /(.jpg|.jpeg|.png)$/i
 
-  if (!allowedExtensions.exec(filePath)) {
+  if (!allowedExtensions.exec(filePath)) 
+  {
     alert("Suba un archivo con alguna de las siguientes extensiones: .jpg/.jpeg/.png")
-    image.value = ''
-    return false;
-  } else {
-    if (image.files && image.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        imagePreview.innerHTML = '<img src="' + e.target.result + '"/>';
+    image.value = ""
+    return false
+  } 
+  else 
+  {
+    if (image.files && image.files[0]) 
+    {
+      var reader = new FileReader()
+      reader.onload = function (e) 
+      {
+        imagePreview.innerHTML = '<img src="' + e.target.result + '"/>'
       }
-      reader.readAsDataURL(image.files[0]);
+      reader.readAsDataURL(image.files[0])
     }
   }
 }
