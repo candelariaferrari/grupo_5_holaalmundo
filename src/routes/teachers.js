86f9c2a1f66a53793f20057d65ff6070365ff731
Martina Vissani <91103968+martinavsn@users.noreply.github.com>
2022-06-01 19:52:26 -0300

    actualizo teachers

--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -1,26 +1,32 @@
-// ************ Require's ************
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-const path = require('path');
-const validator = require('../middlewares/express-validator');
-// ************ Controller Require ************
-const teacherController = require('../controllers/teachersController');
-const authMiddlewars = require('../middlewares/authMiddleware');
+//*** Constantes ***/
+const express = require("express")
+const router = express.Router()
+const multer = require("multer")
+const path = require("path")
 
-/* Profes */
-router.get('/home', authMiddlewars, teacherController.home); //funciona
-router.get('/students', teacherController.students); //funciona
+const validator = require("../middlewares/express-validator")
+const teacherController = require("../controllers/teachersController")
+const authMiddlewars = require("../middlewares/authMiddleware")
+   
+/*** Inicio de profesores ***/
+router.get("/home", authMiddlewars, teacherController.home)
 
-router.get('/packages',teacherController.packages); //funcona
-router.post('/packages', validator.createPackages,teacherController.processPackages);
+/*** Todos los alumnos ***/
+router.get("/students", teacherController.students)
 
-router.get('/configuration', authMiddlewars, teacherController.configuration); 
-router.put('/configuration/:id', validator.configurationTeachers, teacherController.configurationProcess)
+/*** Creación de paquetes ***/
+router.get("/packages", teacherController.packages)
+router.post("/packages", validator.createPackages, teacherController.processPackages)
 
-router.get('/dashboardLessons', teacherController.dashboardLessons);//funciona
-/*** LOGOUT */
-router.get('/logout', teacherController.logout);
+/*** Configuración de estudiantes ***/
+router.get("/configuration", authMiddlewars, teacherController.configuration)
+router.put("/configuration/:id", validator.configurationTeachers, teacherController.configurationProcess)
 
-router.get("/students/:id", authMiddlewars, teacherController.detailsStudent);
-module.exports = router;
+/*** Clases del día ***/
+router.get("/dashboardLessons", teacherController.dashboardLessons)
+
+/*** Cerrar sesión ***/
+router.get("/logout", teacherController.logout)
+
+module.exports = router
+
