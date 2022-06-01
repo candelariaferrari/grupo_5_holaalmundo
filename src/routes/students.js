b788cd3baef24ceaa58f396731d6137d2fe0949c
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-06-01 16:08:46 -0500

    Se ajusta el controlador de student

--- src/routes/students.js
+++ src/routes/students.js
@@ -1,9 +1,10 @@
 // ************ Require's ************
 const express = require("express");
 const router = express.Router();
-const studentsController = require('../controllers/studentsController');
-const authMiddlewars = require('../middlewares/authMiddleware');
-const validator = require('../middlewares/express-validator');
+const studentsController = require("../controllers/studentsController");
+const authMiddlewars = require("../middlewares/authMiddleware");
+const validator = require("../middlewares/express-validator");
+const upload = require('../middlewares/multer/multerRegister');
 
 //const adminMiddlware = require('../middlewares/adminMiddleware');
 
@@ -16,28 +17,24 @@ router.post("/home", authMiddlewars, studentsController.createComment);
 /*** GET PROFESORES */
 router.get("/teachers", authMiddlewars, studentsController.filterTeachers); // authMiddlewars,
 
-
 /*** GET ALL PROFESORES  */
 router.get("/packages", authMiddlewars, studentsController.services); // authMiddlewars
 
 /*** PROFIL PROFESORES */
 
-router.get('/configuration', authMiddlewars, studentsController.configuration); 
-router.post('/configuration', validator.configurationStudents, studentsController.configurationProcess)
+router.get("/configuration", authMiddlewars, studentsController.configuration);
 
-/*** SHOPPING CART  */
-router.get("/shoppingCart", authMiddlewars, studentsController.shoppingCart);
-
-/*** DETAILS TEACHER  */
-/* router.get(
-  "/detailsTeacher",
-  authMiddlewars,
-  studentsController.detailsTeacher
-); */
-
+router.post(
+  "/configuration",
+  upload.any(),
+  validator.configurationStudents,
+  studentsController.configurationProcess
+);
 
 /*** LOGOUT */
 router.get("/logout", studentsController.logout);
 
+/*** DETAILS TEACHER  */
 router.get("/teachers/:id", authMiddlewars, studentsController.detailsTeacher);
+
 module.exports = router;
