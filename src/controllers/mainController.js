84cd1ecf84aa4afe4329b28fa475ecf0f9b45007
Candelaria Ferrari <ferrari.candelaria@gmail.com>
2022-06-01 23:08:33 -0300

    Login - Homes

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -9,83 +9,96 @@ const sequelize = db.sequelize;
 const { Op } = require("sequelize");
 
 let mainController = {
+  home: function (req, res) {
+    //const servicios2 = serviciosService.findAllServices();
+    //const profesores = profesoresServices.findAllTeachers();
+    const servicios = db.Class.findAll({
+      // SELECT * FROM CLASS
+      attributes: ['description', 'language', 'price']
+    });
+    const cantidadTemas = db.Class.findAll(
+      {
+        // SELECT COUNTDISTINCT(topics) AS countTopics FROM Class
+        attributes: [
+          [sequelize.fn('DISTINCT', sequelize.col(`topics`)), `countTopics`],
+        ]
+      }
+    );
+    const cantidadIdiomas = db.Class.findAll(
+      {
+        // SELECT COUNTDISTINCT(language) AS countLanguage FROM Class
+        attributes: [
+          [sequelize.fn('DISTINCT', sequelize.col(`language`)), `countLanguage`]
+        ]
+      }
+    );
+    const profesores = db.User.findAll(
+      {
+        // TODO more queries
+        // SELECT r.description FROM USER 
+        // INNER JOIN ROL as r ON user.user_rol_fk = rol.id
+        include: [
+          {
+            association: "rol"
+          }
+        ],
+        where: {
+          "$rol.description$": { [Op.like]: '%' + "Profesor" + '%' }
+        }
+      }
+    );
+    const alumnos = db.User.findAll(
+      {
+        // TODO more queries
+        // SELECT r.description FROM USER 
+        // INNER JOIN ROL as r ON user.user_rol_fk = rol.id
+        include: [
+          {
+            association: "rol"
+          }
+        ],
+        where: {
+          "$rol.description$": { [Op.like]: '%' + "Estudiante" + '%' }
+        }
+      }
+    );
+    Promise.all([profesores,alumnos, cantidadIdiomas, cantidadTemas, servicios])
+      .then(function ([profesores,alumnos, cantidadIdiomas, cantidadTemas, servicios]) {
 
-    home: function(req, res) {
-       //const servicios2 = serviciosService.findAllServices();
-       //const profesores = profesoresServices.findAllTeachers();
-       
-       const servicios = db.Class.findAll({
+        let data = {
+          profesores: profesores,
+          alumnos,alumnos,
+          cantidadIdiomas: cantidadIdiomas,
+          cantidadTemas: cantidadTemas,
+          servicios: servicios
+        }
 
-            // SELECT * FROM CLASS
-            attributes: ['description', 'language', 'price']
+        res.render('homeGuest/homeGuest', { data: data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  createUserCarusel: function (req, res) {
+    // userServices.createUserCarusel(req, res);
+    // TODO 
+    res.redirect('/users/register');
 
-       });
-        const cantidadTemas = db.Class.findAll(
-            {
-                // SELECT COUNTDISTINCT(topics) AS countTopics FROM Class
-                attributes: [
-                    [sequelize.fn('DISTINCT', sequelize.col(`topics`)), `countTopics`],
-                ]
-            }
-        );
-        const cantidadIdiomas = db.Class.findAll(
-            {
-                // SELECT COUNTDISTINCT(language) AS countLanguage FROM Class
-                attributes: [
-                    [sequelize.fn('DISTINCT', sequelize.col(`language`)), `countLanguage`]
-               ]
-            }
-        );
-        const profesores = db.User.findAll(
-            {
-                // TODO more queries
-                // SELECT r.description FROM USER 
-                // INNER JOIN ROL as r ON user.user_rol_fk = rol.id
-                include: [
-                        {
-                            association: "rol"
-                        }
-                ],
-                where: {
-                    "$rol.description$": { [Op.like]: '%' + "Profesor" + '%'}
-                }
-            }
-        );
-        Promise.all([profesores, cantidadIdiomas, cantidadTemas, servicios])
-            .then(function([profesores, cantidadIdiomas, cantidadTemas, servicios]){
-                
-                let data = {
-                    profesores: profesores, 
-                    cantidadIdiomas: cantidadIdiomas, 
-                    cantidadTemas: cantidadTemas, 
-                    servicios: servicios}
-                
-                res.render('homeGuest/homeGuest', {data: data});
-            })
-            .catch(error => {
-                console.log(error);
-        });
-    },
-    createUserCarusel: function(req, res) {
-        // userServices.createUserCarusel(req, res);
-        // TODO 
-        res.redirect('/users/register');
-        
-    },
-    createComment: function(req, res){
-        // commentService.createComment(req);
-        db.Comment.create({
-            name: req.body.nameContact,
-            email: req.body.emailContact,
-            description: req.body.consultaContact
-        })
-        .then(() =>{
-            res.redirect('/');
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }
+  },
+  createComment: function (req, res) {
+    // commentService.createComment(req);
+    db.Comment.create({
+      name: req.body.nameContact,
+      email: req.body.emailContact,
+      description: req.body.consultaContact
+    })
+      .then(() => {
+        res.redirect('/');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
 
 module.exports = mainController;
